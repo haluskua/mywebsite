d75cbdc374875a1247f3cd101ef27c36eed334f3
haluskua <haluskua.one@gmail.com>
2020-03-13 00:33:51 +1300

    Updates

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".menu-nav__item");
 
+//JAVASCRIPT DEMONSTRATION
+
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
